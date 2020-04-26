c41028bff8799d47a0f3976904f86c02e49b8312
Jingdobi <porterthemerrell@gmail.com>
2020-04-25 19:52:37 -0600

    Update

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -20,6 +20,19 @@ namespace ConsoleApp
 
             Console.WriteLine("Press any key to quit");
             Console.ReadKey();
+
+            /* Date */
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"The current date is: {currentDate.ToShortDateString() }");
+
+            /* Christmas */
+            var christmasDay = new DateTime(currentDate.Year, 12, 25);
+            Console.WriteLine($"Christmas is on: {christmasDay.ToShortTimeString() }");
+
+            var timeSpan = christmasDay - currentDate;
+            var numberOfDaysUntilChristmas = timeSpan.Days;
+            Console.WriteLine($"The number of days utnil Christmas: {numberOfDaysUntilChristmas}");
+
         }
     }
 }
